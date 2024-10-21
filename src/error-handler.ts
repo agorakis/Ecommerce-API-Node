74a0f766cc2ac8f2c68c026ba0ba1610430dba19
Antonis Gorakis <agorakis@gmail.com>
2024-10-21 09:49:44 +0300

    Handling Zod Validation Errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ErrorCode, HttpException } from "./exceptions/root";
 import { InternalException } from "./exceptions/internal-exception";
+import { ZodError } from "zod";
+import { BadRequestsException } from "./exceptions/bad-requests";
 
 export const errorHandler = (method: Function) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -11,11 +13,19 @@ export const errorHandler = (method: Function) => {
       if (error instanceof HttpException) {
         exception = error;
       } else {
-        exception = new InternalException(
-          "Something went wrong",
-          ErrorCode.INTERNAL_EXCEPTION,
-          error
-        );
+        if (error instanceof ZodError) {
+          exception = new BadRequestsException(
+            "Unprocessable entity",
+            ErrorCode.UNPROCESSABLE_ENTITY,
+            error
+          );
+        } else {
+          exception = new InternalException(
+            "Something went wrong",
+            ErrorCode.INTERNAL_EXCEPTION,
+            error
+          );
+        }
       }
       next(exception);
     }
